b60357e739917120d4d28369bb49478dbdc2e103
ramo798 <knht31i@gmail.com>
2021-12-18 01:07:23 +0900

    環境変数の設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  typescript: {
-    strict: true
-  }
-})
+    typescript: {
+        strict: true,
+    },
+    privateRuntimeConfig: {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+    },
+});
