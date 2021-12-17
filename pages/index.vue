d0428ed9c1f77a09a930dcf2b64e67db1a6b4fac
ramo798 <knht31i@gmail.com>
2021-12-18 01:57:13 +0900

    受け取る変数名を変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 // const { data } = useAsyncData('getTest', () => $fetch('/api/gettest'));
-const { data } = await useFetch('/api/gettest');
+const { data: tasks } = await useFetch('/api/gettest');
 </script>
 
 <template>
     <div>
         <p>test</p>
-        <div v-for="task in data" :key="task.id">
+        <div v-for="task in tasks" :key="task.id">
             <p>{{ task.id }}</p>
             <p>{{ task.name }}</p>
         </div>
